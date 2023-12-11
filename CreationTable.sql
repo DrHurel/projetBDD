b64249ac8f1ba510671a1df03c29c3c7c874006b
vincent-bernardon <72875014+vincent-bernardon@users.noreply.github.com>
2023-12-11 16:01:00 +0100

    Update CreationTable.sql

--- CreationTable.sql
+++ CreationTable.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE Proprietaire (
     id_proprietaire INT,
     CONSTRAINT PK_Proprietaire_id_proprietaire PRIMARY KEY(id_proprietaire)
@@ -13,29 +12,24 @@ CREATE TABLE Modele_Objet(
     CONSTRAINT PK_Modele_Objet_id_objet PRIMARY KEY(id_objet)
 );
 
-
 CREATE TABLE Entreprise(
     id_entreprise INT,
     nom_entreprise VARCHAR(50),
     data_creation DATE,
     CONSTRAINT FK_Entreprise_id_entreprise FOREIGN KEY(id_entreprise) REFERENCES Proprietaire(id_proprietaire),
     CONSTRAINT PK_Entreprise PRIMARY KEY(id_entreprise)
-    
 ); 
 
 CREATE TABLE Entreprise_Vaisseau(
     id_entreprise INT,
     CONSTRAINT FK_Entreprise_Vaisseau_id_entreprise FOREIGN KEY(id_entreprise) REFERENCES Entreprise(id_entreprise),
     CONSTRAINT PK_Entreprise_Vaisseau PRIMARY KEY(id_entreprise)
-    
-
 );
 
 CREATE TABLE Entreprise_Objet(
     id_entreprise INT,
     CONSTRAINT FK_Entreprise_Objet_id_entreprise FOREIGN KEY(id_entreprise) REFERENCES Entreprise(id_entreprise),
     CONSTRAINT PK_Entreprise_Objet PRIMARY KEY(id_entreprise)
-    
 );
 
 CREATE TABLE Equipage (
@@ -56,7 +50,6 @@ CREATE TABLE Personne(
     date_naissance DATE,
     id_entreprise INT,
     id_equipage INT,
-
     CONSTRAINT FK_Personne_id_proprietaire FOREIGN KEY(id_personne) REFERENCES Proprietaire(id_proprietaire),
     CONSTRAINT PK_Personne PRIMARY KEY(id_personne),
     CONSTRAINT FK_Personne_id_Entreprise FOREIGN KEY(id_entreprise) REFERENCES Entreprise(id_entreprise),
@@ -70,7 +63,6 @@ CREATE TABLE Vaisseau(
     longueur INT,
     largeur INT,
     id_fabriquant INT,
-
     CONSTRAINT PK_Vaisseau PRIMARY KEY(id_vaisseau),
     CONSTRAINT FK_Vaisseau_id_fabriquant FOREIGN KEY(id_fabriquant) REFERENCES Entreprise_Vaisseau(id_entreprise)
 );
@@ -83,8 +75,6 @@ CREATE TABLE Chef_Entreprise(
     CONSTRAINT FK_Chef_Entreprise_id_personne FOREIGN KEY (id_personne) REFERENCES Personne(id_personne),
     CONSTRAINT FK_Chef_Entreprise_ID_Entreprise FOREIGN KEY (id_entreprise) REFERENCES Entreprise(id_entreprise),
     CONSTRAINT PK_Chef_Entreprise PRIMARY KEY(id_entreprise,id_personne,date_debut)   
-    
-    
 );
 
 CREATE TABLE Gamme_Vente_Objet(
@@ -95,7 +85,6 @@ CREATE TABLE Gamme_Vente_Objet(
     CONSTRAINT PK_Gamme_Vente_Objet PRIMARY KEY(id_objet,id_fabriquant)   
 );
 
-
 CREATE TABLE Inventaire_Vaisseau(
     id_vaisseau INT,
     id_objet INT,
@@ -105,20 +94,14 @@ CREATE TABLE Inventaire_Vaisseau(
     CONSTRAINT PK_Inventaire_Vaisseau PRIMARY KEY(id_objet,id_vaisseau)   
 ); 
 
-
-
-
 CREATE TABLE Historique_Proprio(
     id_vaisseau INT,
     id_proprietaire INT,
     date_debut DATE,
     date_fin DATE,
-
     CONSTRAINT PK_Historique_Proprio PRIMARY KEY(date_debut,id_proprietaire,id_vaisseau),
     CONSTRAINT FK_Historique_Proprio_id_vaisseau FOREIGN KEY(id_vaisseau) REFERENCES Vaisseau(id_vaisseau),
     CONSTRAINT FK_Historique_Proprio_id_proprietaire FOREIGN KEY(id_proprietaire) REFERENCES Proprietaire(id_proprietaire)
-    
-    
 );
 
 CREATE TABLE Historique_Vente_Vaisseau(
