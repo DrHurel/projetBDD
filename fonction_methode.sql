399273ddc1d379ff8101e1aab077704968267d5f
vincent-bernardon <72875014+vincent-bernardon@users.noreply.github.com>
2023-12-14 11:16:46 +0100

    Update fonction_methode.sql
    update de afficher_informations_equipage
    
    création de afficher_informations_equipage_de_tout_les_vaisseaux

--- fonction_methode.sql
+++ fonction_methode.sql
@@ -13,6 +13,7 @@ BEGIN
     -- Vérifier si un équipage est associé à ce vaisseau
     IF equipage_id IS NOT NULL THEN
         -- Afficher les informations des membres de l'équipage sous forme de tableau
+        RAISE NOTICE '+----------------------------Equipage---------------------------------+';
         RAISE NOTICE '+------+-------------+----------+-----------+------+------------------+';
         RAISE NOTICE '| ID   | Nom         | Prénom   | Poste     | Âge  | Date de naissance|';
         RAISE NOTICE '+------+-------------+----------+-----------+------+------------------+';
@@ -30,8 +31,54 @@ BEGIN
                 personne_info.date_naissance;
         END LOOP;
         RAISE NOTICE '+------+-------------+----------+-----------+------+------------------+';
+        RAISE NOTICE ' ';
+        RAISE NOTICE ' ';
+        RAISE NOTICE ' ';
+
     ELSE
         RAISE NOTICE 'Ce vaisseau n''est associé à aucun équipage.';
+        RAISE NOTICE ' ';
+        RAISE NOTICE ' ';
+        RAISE NOTICE ' ';
+
     END IF;
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE afficher_informations_equipage_de_tout_les_vaisseaux()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    vaisseaux_info RECORD;
+    equipage_bool BOOLEAN := FALSE;
+BEGIN
+    FOR vaisseaux_info IN
+        SELECT id_vaisseau, nom, prix, masse, longueur, largeur, id_fabriquant
+        FROM Vaisseau
+    LOOP
+        SELECT TRUE INTO equipage_bool 
+        FROM equipage 
+        WHERE id_vaisseau=vaisseaux_info.id_vaisseau;
+
+        IF equipage_bool THEN
+            RAISE NOTICE '+----------------------------Vaisseau---------------------------------+';
+            RAISE NOTICE '+--------------------------%-----------------------------+',
+                lpad(vaisseaux_info.nom, 10, ' ');
+            RAISE NOTICE '+------+-------------+----------+-----------+---------+---------------+';
+            RAISE NOTICE '| ID   | Prix        | Masse    | Longueur  | Largeur | ID_fabriquant |';
+            RAISE NOTICE '+------+-------------+----------+-----------+---------+---------------+';
+            RAISE NOTICE '| % | % | % | % | % | % |',
+                lpad(vaisseaux_info.id_vaisseau::text, 4, ' '),
+                lpad(vaisseaux_info.prix::text, 10, ' '),
+                lpad(vaisseaux_info.masse::text, 7, ' '),
+                lpad(vaisseaux_info.longueur::text, 8, ' '),
+                lpad(vaisseaux_info.largeur::text, 6, ' '),
+                lpad(vaisseaux_info.id_fabriquant::text, 12, ' ');
+            CALL afficher_informations_equipage(vaisseaux_info.id_vaisseau);
+        ELSE
+            RAISE NOTICE '|Aucun équipage pour ce vaisseau|';
+        END IF;
+    END LOOP;
+    RAISE NOTICE '+------+-------------+----------+-----------+--------+----------------+';
 END;
 $$;
