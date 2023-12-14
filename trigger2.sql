1ffd888ec932ba984324f693498197329023b9e9
DrHurel <jeremyhurel34@gmail.com>
2023-12-14 10:01:26 +0100

    fix trigger

--- trigger2.sql
+++ trigger2.sql
@@ -8,15 +8,15 @@ DECLARE
     jour_current INTEGER;
     
 BEGIN
-    SELECT YEAR(CURRENT_DATE),MONTH(CURRENT_DATE),DAY(CURRENT_DATE) INTO annee_current,mois_current,jour_current;
+    SELECT EXTRACT(YEAR FROM CURRENT_DATE),EXTRACT(MONTH FROM CURRENT_DATE),EXTRACT(DAY FROM CURRENT_DATE) INTO annee_current,mois_current,jour_current;
     
-    age_result := annee_current-YEAR(NEW.date_naissance);
-    IF mois_current> MONTH(NEW.date_naissance) THEN
+    age_result := annee_current- EXTRACT(YEAR FROM NEW.date_naissance);
+    IF mois_current> EXTRACT(MONTH FROM NEW.date_naissance) THEN
         NEW.age := age_result -1;
         RETURN NEW;
     END IF;
 
-    IF mois_current== MONTH(NEW.date_naissance) AND jour_current> DAY(NEW.date_naissance) THEN
+    IF mois_current= EXTRACT(MONTH FROM NEW.date_naissance)  AND jour_current> EXTRACT(DAY FROM NEW.date_naissance) THEN
         NEW.age := age_result -1;
         RETURN NEW;
     END IF;
@@ -25,7 +25,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Créer le déclencheur
-CREATE TRIGGER trigger_update_age
+CREATE OR REPLACE TRIGGER trigger_update_age
 BEFORE INSERT OR UPDATE ON Personne
 FOR EACH ROW
 EXECUTE FUNCTION update_age();
