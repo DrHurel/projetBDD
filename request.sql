c47957254ae8114d8ee69f03818be7bb6e5a5fa8
DrHurel <jeremyhurel34@gmail.com>
2023-12-16 14:51:07 +0100

    save working request TODO division & 1more

--- request.sql
+++ request.sql
@@ -1,5 +1,11 @@
-SELECT Count(*) FROM Personne GROUP BY id_entreprise; -- Nombre de personne dans une entreprise
+SELECT id_entreprise,Count(*) FROM Personne GROUP BY id_entreprise; -- Nombre de personne dans une entreprise
 
-SELECT * FROM Vaisseau WHERE id_vaisseau IN SELECT id_vaisseau FROM Equipage GROUP BY id_vaisseau; -- tout les vaisseau qui on un équipage
+SELECT * FROM Vaisseau WHERE id_vaisseau IN (SELECT id_vaisseau FROM Equipage GROUP BY id_vaisseau); -- tout les vaisseau qui on un équipage
 
-SELECT * FROM Entreprise WHERE id_entreprise IN SELECT * FROM Entreprise_Objet eo WHERE EXISTS (SELECT * FROM Modele_Objet mo WHERE eo.id_entreprise=mo.id_entreprise AND mo.statut='Illegal'); -- tout les entreprise objet qui vendent des choses illégal 
+SELECT * FROM Entreprise 
+WHERE id_entreprise 
+IN (
+    SELECT * FROM Entreprise_Objet AS eo 
+    WHERE EXISTS (
+        SELECT * FROM Gamme_Vente_Objet AS gvo JOIN Modele_Objet AS mo ON gvo.id_objet=mo.id_objet
+        WHERE eo.id_entreprise = gvo.id_fabriquant AND mo.statut='ILLEGAL')); -- tout les entreprise objet qui vendent des choses illégal 
