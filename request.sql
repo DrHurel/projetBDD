28301337af02d105dd560d80204238b8ef58040a
DrHurel <jeremyhurel34@gmail.com>
2023-12-16 14:40:53 +0100

    WIP Requetes

--- request.sql
+++ request.sql
@@ -1,1 +1,5 @@
-SELECT * FROM Entreprise_Object WHERE id_entreprise IN SELECT id_entreprise FROM Entreprise_Objet JOIN Gamme_Vente_Objet ON id_fabriquant=id_entreprise JOIN Modele_Objet ON Modele_Objet.id_objet=Gamme_Vente_Objet.id_objet WHERE statut<>"Illegal";
+SELECT Count(*) FROM Personne GROUP BY id_entreprise; -- Nombre de personne dans une entreprise
+
+SELECT * FROM Vaisseau WHERE id_vaisseau IN SELECT id_vaisseau FROM Equipage GROUP BY id_vaisseau; -- tout les vaisseau qui on un équipage
+
+SELECT * FROM Entreprise WHERE id_entreprise IN SELECT * FROM Entreprise_Objet eo WHERE EXISTS (SELECT * FROM Modele_Objet mo WHERE eo.id_entreprise=mo.id_entreprise AND mo.statut='Illegal'); -- tout les entreprise objet qui vendent des choses illégal 
